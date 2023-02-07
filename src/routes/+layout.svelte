245a48d52cc3517b848e98c92e412c3c4fd568e5
Floki <dawysonw@gmail.com>
2023-02-07 14:46:37 -0300

    update title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.postcss';
 </script>
-
+<svelte:head>
+    <title>GDUtils</title>
+</svelte:head>
 <slot />
 <footer class="absolute bottom-0 p-1 left-0 right-0 text-center font-thin text-xs">
     <div> with 💜 by flokitv</div>
